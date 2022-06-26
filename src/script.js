31ef74a9855b9b858efa9e579e0e953c824c5126
Nando <fmfernando61@gmail.com>
2022-06-26 18:10:47 -0300

    upload source

--- src/script.js
+++ src/script.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
-const getBanco = () => JSON.parse(localStorage.getItem("todoList")) ?? [];
-const setBancp = (banco) =>
-  localStorage.setItem("todoList", JSON.stringify(banco));
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setBanco = (banco) =>
+  localStorage.setItem('todoList', JSON.stringify(banco));
 
 const criarItem = (tarefa, status, indice) => {
-  const item = document.createElement("label");
-  item.classList.add("todo__item");
+  const item = document.createElement('label');
+  item.classList.add('todo__item');
   item.innerHTML = `
     <input type="checkbox" ${status} data-indice=${indice}>
     <div>${tarefa}</div>
     <input type="button" value="X" data-indice=${indice}>
   `;
 
-  document.getElementById("todoList").appendChild(item);
+  document.getElementById('todoList').appendChild(item);
 };
 
 const limparTarefas = () => {
-  const todoList = document.getElementById("todoList");
+  const todoList = document.getElementById('todoList');
   while (todoList.firstChild) {
     todoList.removeChild(todoList.lastChild);
   }
@@ -33,12 +33,12 @@ const inserirItem = (evento) => {
   const tecla = evento.key;
   const texto = evento.target.value;
 
-  if (tecla === "Enter") {
+  if (tecla === 'Enter') {
     const banco = getBanco();
-    banco.push({ tarefa: texto, status: "" });
+    banco.push({ tarefa: texto, status: '' });
     setBanco(banco);
     atualizarTela();
-    evento.target.value = "";
+    evento.target.value = '';
   }
 };
 
@@ -51,23 +51,23 @@ const removerItem = (indice) => {
 
 const atualizarItem = (indice) => {
   const banco = getBanco();
-  banco[indice].status = banco[indice].status === "" ? "checked" : "";
+  banco[indice].status = banco[indice].status === '' ? 'checked' : '';
   setBanco(banco);
   atualizarTela();
 };
 
 const clickItem = (evento) => {
   const elemento = evento.target;
-  if (elemento.type === "button") {
+  if (elemento.type === 'button') {
     const indice = elemento.dataset.indice;
     removerItem(indice);
-  } else if (elemento.type === "checkbox") {
+  } else if (elemento.type === 'checkbox') {
     const indice = elemento.dataset.indice;
     atualizarItem();
   }
 };
 
-document.getElementById("newItem").addEventListener("keypress", inserirItem);
-document.getElementById("todoList").addEventListener("click", clickItem);
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
 
 atualizarTela();
